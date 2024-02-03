eba970395de0bf674ab3dca3af091337ed1d7151
andrzmc <drzxax@gmail.com>
2024-02-03 14:33:22 -0600

    chore: install react navigation to create routes and screens

--- src/components/modules/global/global-configuration.tsx
+++ src/components/modules/global/global-configuration.tsx
@@ -1,21 +1,29 @@
 import {ChildrenProps} from '@/models/interfaces/global/children';
 import {ColorLightStyle, ColorPrimaryStyle} from '@/styles/global/colors';
-import React, {FC} from 'react';
-import {SafeAreaView, useColorScheme} from 'react-native';
+import React, {FC, useEffect, useState} from 'react';
+import {View, useColorScheme} from 'react-native';
 
 const GlobalConfigurationModule: FC<ChildrenProps> = props => {
   const {children} = props;
 
   const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <SafeAreaView
-      style={[
-        {backgroundColor: isDarkMode ? ColorPrimaryStyle : ColorLightStyle},
-      ]}>
-      {children}
-    </SafeAreaView>
-  );
+  const [backgroundColor, setBackgroundColor] = useState<string>();
+
+  useEffect(() => {
+    init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const init = () => {
+    defineColorMode();
+  };
+
+  const defineColorMode = () => {
+    setBackgroundColor(isDarkMode ? ColorPrimaryStyle : ColorLightStyle);
+  };
+
+  return <View style={{backgroundColor}}>{children}</View>;
 };
 
 export default GlobalConfigurationModule;
