c8a923aaf00e335b1975fa7fe812e8cc50bdb27b
andrzmc <drzxax@gmail.com>
2024-02-02 19:51:00 -0600

    chore: add configuration to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['import', 'react', 'prettier', '@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-console': 'warn',
+        'no-eval': 'error',
+        'no-unused-vars': 'warn',
+        'import/first': 'error',
+        'react/prop-types': 'error',
+        'react/display-name': 0,
+        'no-mixed-spaces-and-tabs': 'error',
+        'react/no-unescaped-entities': 0,
+        'react/jsx-no-duplicate-props': 'error',
+        'react/react-in-jsx-scope': 0,
+      },
+    },
+  ],
+
 };
