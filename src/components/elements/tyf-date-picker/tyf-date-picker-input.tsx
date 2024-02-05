be6f15a89a116aad4b2292358c9e974aec7bbafc
andrzmc <drzxax@gmail.com>
2024-02-05 13:17:26 -0600

    fix: color border date picker input

--- src/components/elements/tyf-date-picker/tyf-date-picker-input.tsx
+++ src/components/elements/tyf-date-picker/tyf-date-picker-input.tsx
@@ -4,17 +4,21 @@ import {
 } from '@/styles/components/elements/tyf-date-picker';
 import React, {FC} from 'react';
 import TyfTypography from '../tyf-typography/tyf-typography';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, useColorScheme} from 'react-native';
 import moment from 'moment';
 import {CalendarBlank} from 'phosphor-react-native';
 import {TyfDatePickerContentProps} from '@/models/interfaces/components/elements/tyf-date-picker';
+import {ColorLightStyle, ColorPrimaryStyle} from '@/styles/global/colors';
 
 const TyfDatePickerInput: FC<TyfDatePickerContentProps> = props => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   const {value, placeholder, disabled, onPress} = props;
 
   return (
     <TouchableOpacity onPress={onPress} disabled={disabled}>
-      <TyfDatePickerInputStyled>
+      <TyfDatePickerInputStyled
+        style={{borderColor: isDarkMode ? ColorLightStyle : ColorPrimaryStyle}}>
         <TyfTypography
           text={placeholder}
           variant={value ? 'Small' : 'Paragraph'}
