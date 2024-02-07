510d5376a3f20a8834d0abbd9c30b7f5c843cf58
andrzmc <drzxax@gmail.com>
2024-02-06 21:33:30 -0600

    feat: prepare screen FlightDetails

--- src/screens/flight/details.tsx
+++ src/screens/flight/details.tsx
@@ -1,10 +1,21 @@
 import TyfTypography from '@/elements/tyf-typography/tyf-typography';
 import useNavigate from '@/hooks/use-navigate';
-import React, {useEffect} from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ImageBackground, TouchableOpacity, View} from 'react-native';
+import {SwipeablePanel} from 'rn-swipeable-panel';
 
 const FlightDetailScreen = () => {
   const {getScreenParams} = useNavigate();
+
+  const [isPanelActive, setIsPanelActive] = useState(false);
+
+  const openPanel = () => {
+    setIsPanelActive(true);
+  };
+
+  const closePanel = () => {
+    setIsPanelActive(false);
+  };
 
   useEffect(() => {
     console.log(getScreenParams());
@@ -12,7 +23,23 @@ const FlightDetailScreen = () => {
 
   return (
     <View>
-      <TyfTypography text="FlightDetailScreen" />
+      <ImageBackground
+        source={require('@/images/MEX.png')}
+        resizeMode="cover"
+        style={{width: '100%', height: 350}}
+      />
+      <TouchableOpacity onPress={openPanel}>
+        <TyfTypography text="FlightDetailScreen" />
+      </TouchableOpacity>
+      <SwipeablePanel
+        fullWidth={true}
+        openLarge={true}
+        showCloseButton
+        smallPanelHeight={350}
+        onClose={closePanel}
+        isActive={isPanelActive}>
+        <TyfTypography text="MY PANEL" />
+      </SwipeablePanel>
     </View>
   );
 };
